927f58e5613fbee7bd4bdab1ceefc21034a58de7
spencer barrett <barrettspencer@outlook.com>
2025-09-04 15:00:37 -0700

    Update react-router.config.ts

--- apps/frontend/guppylabs/react-router.config.ts
+++ apps/frontend/guppylabs/react-router.config.ts
@@ -1,8 +1,17 @@
-import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from "@vercel/react-router/vite";
+import {
+  type RouteConfig,
+  route,
+  index,
+  layout
+} from "@react-router/dev/routes";
 
-export default {
-  appDirectory: "./src/app",
-  ssr: false,
-  presets: [vercelPreset()],
-} satisfies Config;
+
+
+export default [
+  layout("./layouts/app-layout.tsx", [
+      index("./pages/home.tsx"),
+      route("about", "./pages/about.tsx"),
+      route("staff", "./pages/staff.tsx"),
+      route("projects", "./pages/projects.tsx")
+  ]),
+] satisfies RouteConfig
