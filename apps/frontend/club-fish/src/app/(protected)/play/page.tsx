bdc67360fe694315b12d45edf2a1db3a5dc52655
spencer-barrett <barrettspencer@outlook.com>
2025-10-10 13:31:41 -0700

    Update play page to load phaser window

--- apps/frontend/club-fish/src/app/(protected)/play/page.tsx
+++ apps/frontend/club-fish/src/app/(protected)/play/page.tsx
@@ -5,7 +5,12 @@ import { Button } from "@/components/ui/button";
 import type { User } from "firebase/auth";
 import { auth } from "@/lib/firebase/clientApp";
 
+import dynamic from "next/dynamic";
 
+const PhaserCanvas = dynamic(
+    () => import("@/components/phaser/PhaserCanvas"),
+    { ssr: false }
+);
 function PlayPage() {
 
     const handleSignOut = async () => {
@@ -18,10 +23,16 @@ function PlayPage() {
     }
     return (
         <div className="flex sm:h-[calc(100vh-72px)] h-[calc(100vh-60px)] bg-purple-300">
+            <div className="flex flex-col w-full h-full items-center justify-center">
 
-            PLay Page
 
-            <Button onClick={handleSignOut}>Sign Out</Button>
+                <div className="w-[960px] h-[540px] bg-black mb-6">
+                    {/*This is the phaser canvas window!!*/}
+                    <PhaserCanvas width={960} height={540} /> 
+                </div>
+                <Button onClick={handleSignOut}>Sign Out!</Button>
+            </div>
+
         </div>
     );
 }
