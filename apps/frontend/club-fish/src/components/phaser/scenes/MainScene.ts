a9220c0db7a8c8b7b5a140e60ca3a874fad7ffa3
tsmaz <tmazichs@gmail.com>
2025-10-24 03:48:02 -0700

    Initial implementation of movement

--- apps/frontend/club-fish/src/components/phaser/scenes/MainScene.ts
+++ apps/frontend/club-fish/src/components/phaser/scenes/MainScene.ts
@@ -1,8 +1,13 @@
 import Phaser from "phaser";
+import {Math as pMath} from "phaser";
+import Pointer = Phaser.Input.Pointer;
+const {Vector2} = pMath;
 
 export class MainScene extends Phaser.Scene {
+
   fish!: Phaser.GameObjects.Image;
   private bodyColor = "#60cbfcff";
+  target = new Vector2();
 
   init(data: { bodyColor?: string }) {
     console.log("MainScene: init", data);
@@ -27,6 +32,24 @@ export class MainScene extends Phaser.Scene {
     this.add.image(width * 0.5, height * 0.5, "ocean").setOrigin(0.5);
     this.fish = this.add.image(width * 0.5, height * 0.5, key).setScale(0.5);
 
+      // When the user releases the screen...
+      this.input.on('pointerup', (pointer: Pointer) => {
+          // Get the WORLD x and y position of the pointer
+          const {worldX, worldY} = pointer;
+
+          // Assign the world x and y to our vector
+          this.target.x = worldX;
+          this.target.y = worldY;
+
+          // Start moving our cat towards the target
+          this.physics.moveToObject(this.fish, this.target, 200);
+      });
+
+
     console.log("MainScene: create finished");
+
+
   }
+
+
 }
