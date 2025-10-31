10780bf05877054973734ad4575c6b3ce4f6e16e
JoshChiaCPP <jcchia@cpp.edu>
2025-10-30 20:09:08 -0700

    changes to both colyseus room files

--- apps/backend/club-fish-server/src/rooms/MyRoom.ts
+++ apps/backend/club-fish-server/src/rooms/MyRoom.ts
@@ -1,5 +1,5 @@
 import { Room, Client } from "@colyseus/core";
-import { MyRoomState } from "./schema/MyRoomState";
+import { MyRoomState, Player } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
   maxClients = 4;
@@ -10,6 +10,7 @@ export class MyRoom extends Room<MyRoomState> {
 
     // keep room alive even with 0 clients
     this.autoDispose = false;
+    this.state.players;
 
     // optional: mark as public and searchable
     this.setPrivate(false);
@@ -23,14 +24,48 @@ export class MyRoom extends Room<MyRoomState> {
       // handle "type" message
       //
     });
+
+    //handle player input
+    this.onMessage("movement", (client: Client, payload: { x: number, y: number}) => {
+      //get ref to the player who sent the message
+      const player = this.state.players.get(client.sessionId)
+      console.log(`Client ${client.sessionId} sent desired movement: left=${payload.x}, right=${payload.y}`);
+      player.x = payload.x;
+      player.y = payload.y;
+      
+      //now broadcast this movement to all clients
+      this.broadcast("someone-moved", {
+        id: client.sessionId,
+        x: payload.x,
+        y: payload.y
+      });
+    });
+
+    //send leave message back to client so it can access it's own id
+    this.onMessage("i-joined", (client: Client, message: any) => {
+      client.send("your-id", client.sessionId)
+      console.log("key color", message)
+      //broadcast player joining
+      const payload = {
+        id: client.sessionId,
+        key: message
+      };
+      this.broadcast("someone-joined", payload, { except: client});
+    });
+    this.onMessage("i-left", (client: Client, message: any) => {
+      this.broadcast("someone-left", client.sessionId)
+    });
   }
 
   onJoin(client: Client, options: any) {
     console.log(client.sessionId, "joined!");
+    const player = new Player();
+    this.state.players.set(client.sessionId, player);
   }
 
   onLeave(client: Client, consented: boolean) {
     console.log(client.sessionId, "left!");
+    this.state.players.delete(client.sessionId);
   }
 
   onDispose() {
--- apps/backend/club-fish-server/src/rooms/schema/MyRoomState.ts
+++ apps/backend/club-fish-server/src/rooms/schema/MyRoomState.ts
@@ -1,7 +1,12 @@
-import { Schema, type } from "@colyseus/schema";
+import { MapSchema, Schema, type } from "@colyseus/schema";
 
+export class Player extends Schema {
+  @type("number") x: number;
+  @type("number") y: number;
+}
 export class MyRoomState extends Schema {
-
+  @type({ map: Player })
+  players = new MapSchema<Player>();
   @type("string") mySynchronizedProperty: string = "Hello world";
 
 }
