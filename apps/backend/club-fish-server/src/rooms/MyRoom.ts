c24af8cd8b95fe0c4a1da8826db02dbb212478b7
spencer-barrett <barrettspencer@outlook.com>
2025-11-04 15:49:05 -0800

    Major refactor to listen to state manipulation rather than message listening

--- apps/backend/club-fish-server/src/rooms/MyRoom.ts
+++ apps/backend/club-fish-server/src/rooms/MyRoom.ts
@@ -1,23 +1,15 @@
 import { Room, Client } from "@colyseus/core";
-import { MyRoomState, Player } from "./schema/MyRoomState";
+import { InputData, MyRoomState, Player } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
   state = new MyRoomState();
+  fixedTimeStep = 1000 / 60;
+
   onCreate(options: any) {
     this.maxClients = 4;
     console.log("MyRoom created!");
 
-    // keep room alive even with 0 clients
-    this.autoDispose = false;
-    this.onMessage("type", (client, message) => {
-      //
-      // handle "type" message
-      //
-    });
-    this.state.players;
-
-    // optional: mark as public and searchable
-    this.setPrivate(false);
+    
     this.onMessage("chat", (client, message) => {
       console.log(`chat from ${client.sessionId}: ${message.text}`);
       const payload = {
@@ -25,57 +17,60 @@ export class MyRoom extends Room<MyRoomState> {
         sender: message.sender,
       };
       this.broadcast("chat", payload)
-      // handle "type" message
-      //
     });
 
-    //handle player input
-    this.onMessage("movement", (client, payload: { x: number, y: number, k: string}) => {
-      //get ref to the player who sent the message
+    this.onMessage(0, (client, payload) => {
       const player = this.state.players.get(client.sessionId);
-      console.log(`Client ${client.sessionId} sent desired movement: x=${payload.x}, y=${payload.y}`);
-      player.x = payload.x;
-      player.y = payload.y;
+      console.log(`   Received from ${client.sessionId}: (${payload.x}, ${payload.y})`);
 
-      const toSend = {
-        id: client.sessionId,
-        x: payload.x,
-        y: payload.y,
-        key: payload.k
-      };
-      
-      //now broadcast this movement to all clients
-      this.broadcast("someone-moved", toSend);
-    });
+      player.inputQueue.push(payload);
+    })
+let elapsedTime = 0;
+    this.setSimulationInterval((deltaTime) => {
+      elapsedTime += deltaTime;
 
-    //send leave message back to client so it can access it's own id
-    this.onMessage("i-joined", (client, message) => {
-      client.send("your-id", client.sessionId)
-      console.log("key color", message)
-      //broadcast player joining
-      const payload = {
-        id: client.sessionId,
-        key: message
-      };
-      this.broadcast("someone-joined", payload, { except: client});
+      while (elapsedTime >= this.fixedTimeStep) {
+        elapsedTime -= this.fixedTimeStep;
+        this.fixedTick(this.fixedTimeStep);
+      }
     });
-    this.onMessage("i-left", (client) => {
-      this.broadcast("someone-left", client.sessionId)
-    });
+    
+  }
+
+  
+
+  fixedTick(timestep: number){
+    this.state.players.forEach(player => {
+      let input: InputData;
+
+      while(input = player.inputQueue.shift()){
+        player.x = input.x;
+        player.y = input.y;
+        player.tick = input.tick;
+      }
+    })
   }
 
   onJoin(client: Client, options: any) {
     console.log(client.sessionId, "joined!");
     const player = new Player();
+
+    player.x = (Math.random() * 1100);
+    player.y = (Math.random() * 575);
     this.state.players.set(client.sessionId, player);
   }
 
   onLeave(client: Client, consented: boolean) {
     console.log(client.sessionId, "left!");
-    this.state.players.delete(client.sessionId);
+    const player = this.state.players.get(client.sessionId);
+    if (player) {
+        this.state.players.delete(client.sessionId);
+         console.log(`Deleted player ${client.sessionId}`);
   }
+}
 
   onDispose() {
     console.log("room", this.roomId, "disposing...");
+    
   }
 }
