125f7f517bd43ed961a2dbafe836ab55e245528e
spencer-barrett <barrettspencer@outlook.com>
2025-11-06 13:06:36 -0800

    Update url for testing with live-server

--- apps/frontend/club-fish/src/lib/colyseus/networkController.ts
+++ apps/frontend/club-fish/src/lib/colyseus/networkController.ts
@@ -70,6 +70,6 @@ export class NetworkManager {
 
 
 export const networkManager = new NetworkManager(
-  // process.env.NEXT_PUBLIC_COLYSEUS_URL ?? "wss://game.fishfish.io"
-  process.env.NEXT_PUBLIC_COLYSEUS_URL ?? "ws://localhost:2567"
+  process.env.NEXT_PUBLIC_COLYSEUS_URL ?? "wss://game.fishfish.io"
+  // process.env.NEXT_PUBLIC_COLYSEUS_URL ?? "ws://localhost:2567"
 );
